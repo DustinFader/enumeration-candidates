b2bdfd1814dfa6326cf2217a72a38ba007317823
DustinFader <dustinfader16@gmail.com>
2024-03-03 21:20:17 -0400

    implement experienced method when candidate is or above 2 years exprience

--- filters.rb
+++ filters.rb
@@ -1,16 +1,19 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
-
+ 
 def find(id)
-    # Your code Here
-  end
   
-  def experienced?(candidate)
-    # Your code Here
-  end
+end
   
-  def qualified_candidates(candidates)
-    # Your code Here
+def experienced?(candidate)
+  if candidate[:years_of_experience] >= 2
+    return true
   end
+  false
+end
+  
+def qualified_candidates(candidates)
+
+end
   
   # More methods will go below
